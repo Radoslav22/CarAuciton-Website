94b64d5fd41c353f55adfa9078d0c7f46f5d92af
Radoslav22 <4takvr26@solent.ac.uk>
2023-10-19 11:54:01 +0100

    Edited Nav Bar

--- client/src/components/carlist.js
+++ client/src/components/carlist.js
@@ -51,7 +51,7 @@ export default function ComplexGrid() {
 
 
         <Paper
-
+          key={car.id}
           sx={{
             p: 2,
             margin: 'auto',
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,168 +1,163 @@
 import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { AccountCircle, Home, Timer, CalendarToday, PlaylistAddCheck } from '@mui/icons-material';
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import AddBoxIcon from '@mui/icons-material/AddBox';
-import HomeIcon from '@mui/icons-material/Home';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
-const drawerWidth = 240;
-
-
-
-const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-    transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: `${drawerWidth}px`,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }),
-}));
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-
-}));
-
-export default function HeaderMenu() {
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-
-    const [anchorEl, setAnchorEl] = React.useState(null);
+import Auction from '../assets/auction.png';
 
 
-    const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
+
+
+function HeaderMenu() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [anchorAuc, setAnchorAuc] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
     };
 
-    const handleClose = () => {
-        setAnchorEl(null);
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
     };
 
-
-
-    const handleDrawerOpen = () => {
-        setOpen(true);
+    const handleOpenAucMenu = (event) => {
+        setAnchorAuc(event.currentTarget)
     };
 
-    const handleDrawerClose = () => {
-        setOpen(false);
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
     };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const handleCloseAucMenu = () => {
+        setAnchorAuc(null);
+    }
+
+
 
     return (
-        <Box sx={{ display: 'flex' }} >
-            <CssBaseline />
-            <AppBar position="static" open={open} sx={{ alignContent: "center", background: '#325ca8' }}>
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        onClick={handleDrawerOpen}
-                        sx={{ mr: 2, ...(open && { display: 'none' }) }}
-                    >
+        <AppBar position="static" sx={{ background: "white" }}>
+            <Container maxWidth="xl"   >
+                <Toolbar disableGutters sx={{ padding: "0 10% 0 10%" }} >
 
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        CarHub
-                    </Typography>
-                    <Link style={{ textDecoration: "none", color: "white" }} to='/login'>
-                        <Button color="inherit">Login</Button>
-                    </Link>
-                    <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorEl}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                    >
+                    <img src={Auction} alt='logo' style={{ height: "60px", width: "160px" }} />
+                    <Container sx={{ display: "flex", justifyContent: "center", textAlign: "center", margin: "auto" }}>
+                        <Box sx={{ marginRight: "10px" }}>
 
-                    </Menu>
+                            <Button
+
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'black', display: 'block' }}
+                            >
+                                Home
+                            </Button>
+
+                        </Box>
+                        <Box sx={{ display: "inline-block", marginRight: "10px" }} >
+
+                            <Button
+                                onClick={handleOpenAucMenu}
+                                sx={{ my: 2, color: 'black', display: 'block' }}>
+                                Auction slots
+
+                            </Button>
+
+
+                            <Menu
+                                sx={{ mt: '2px' }}
+                                id="menu-appbar"
+                                anchorEl={anchorAuc}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorAuc)}
+                                onClose={handleCloseAucMenu}
+                            >
+
+                                <MenuItem onClick={handleCloseAucMenu}>
+                                    <Typography textAlign="center">Cars</Typography>
+                                </MenuItem>
+                                <MenuItem onClick={handleCloseAucMenu}>
+                                    <Typography textAlign="center">Motorbikes</Typography>
+                                </MenuItem>
+                                <MenuItem onClick={handleCloseAucMenu}>
+                                    <Typography textAlign="center">Commercials</Typography>
+                                </MenuItem>
+                                <MenuItem onClick={handleCloseAucMenu}>
+                                    <Typography textAlign="center">Search</Typography>
+                                </MenuItem>
+
+                            </Menu>
+                        </Box>
+                        <Box sx={{ marginRight: "10px" }}>
+                            <Button
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'black', display: 'block' }}>
+                                Auction times
+                            </Button>
+                        </Box>
+                        <Box sx={{ marginRight: "10px" }}>
+                            <Button
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'black', display: 'block' }}>
+                                Help center
+                            </Button>
+
+                        </Box>
+
+                    </Container>
+                    <Box >
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+
+                            <MenuItem onClick={handleCloseUserMenu}>
+                                <Typography textAlign="center">settings</Typography>
+                            </MenuItem>
+
+                        </Menu>
+                    </Box>
                 </Toolbar>
-            </AppBar>
-            <Drawer
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': {
-                        width: drawerWidth,
-                        boxSizing: 'border-box',
-
-                    },
-                }}
-                variant="persistent"
-                anchor="left"
-                open={open}
-            >
-                <DrawerHeader>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                    </IconButton>
-                </DrawerHeader>
-                <Divider />
-                <List >
-                    <Link style={{ textDecoration: "none" }} to='/'>
-                        <ListItemButton >
-
-                            <HomeIcon sx={{mr:2}}>
-                                <Home fontSize='large' />
-                            </HomeIcon >
-                            <ListItemText sx={{ color: 'black' }}>Home</ListItemText>
-
-                        </ListItemButton>
-                    </Link>
-                    <Link style={{ textDecoration: "none" }} to='/add'>
-                        <ListItemButton>
-                            <AddBoxIcon sx={{mr:2}}>
-                                <Timer fontSize='large' />
-                            </AddBoxIcon >
-                            <ListItemText sx={{ color: 'black' }}>Publish</ListItemText>
-                        </ListItemButton>
-                    </Link>
-                </List>
-                <Divider />
-
-            </Drawer>
-
-        </Box >
+            </Container >
+        </AppBar >
     );
 }
+export default HeaderMenu;
