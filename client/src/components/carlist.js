af20e8e1685bc50b947b0b981c6133f6ea1b3506
Radoslav22 <4takvr26@solent.ac.uk>
2023-10-24 12:46:49 +0100

    Updated displaying of the cars

--- client/src/components/carlist.js
+++ client/src/components/carlist.js
@@ -55,181 +55,116 @@ export default function ComplexGrid() {
     <div>
       {cars.map((car) => (
 
+        <Paper key={car.id} sx={{
+          p: 2,
+          margin: 'auto',
+          marginTop: 2,
+          marginBottom: 2,
+          maxWidth: "80%",
+          flexGrow: 1,
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
 
-        <Paper
-          key={car.id}
-          sx={{
-            p: 2,
-            margin: 'auto',
-            marginTop: 2,
-            marginBottom: 2,
-            maxWidth: "80%",
-            flexGrow: 1,
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-
-          }}
-        >
-
-          <Grid container spacing={2} key={car.id}>
-
-            <Grid item>
-
-              <ButtonBase sx={{ width: "auto", height: "50%", padding: "50 50 50 50" }}>
-
+        }}>
+          <Grid container spacing={2}>
+            <Grid item xs={9} md={3}>
+              <ButtonBase sx={{
+                height: "100%",
+                width: "auto",
+                marginTop: "0px",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center center"
+              }}>
                 <Img src={Carimg} alt="carimg" />
-
               </ButtonBase>
             </Grid>
-
-            <Grid item xs={12} sm container>
-              <Grid item xs container direction="column" spacing={2}>
-                <Grid item xs>
-                  <Typography gutterBottom variant="subtitle1" component="div">
-                    {car.manufacturer} {car.model}
-                  </Typography>
-                  <Typography variant="body2" gutterBottom>
-                    Year: {car.year}, {car.mileage}km
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {car.description}
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Typography sx={{ cursor: 'pointer' }} variant="body2">
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid item>
-                <Typography variant="subtitle1" component="div">
-                  {car.price}.00$
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid sx={{ ml: 68 }}>
-            <Stack direction="row" spacing={2}>
-              <Link style={{ textDecoration: "none" }} to={`/update/${car.id}`}>
-                <Button variant="contained" color="success" >
-                  Edit
-                </Button>
-              </Link>
-              <Button onClick={(e) => handleClick(e, car.car_id)} variant="outlined" color="error">
-                Delete
-              </Button>
-            </Stack>
-
-          </Grid>
-        </Paper >
-      ))}
-
-      <Paper sx={{
-        p: 2,
-        margin: 'auto',
-        marginTop: 2,
-        marginBottom: 2,
-        maxWidth: "80%",
-        flexGrow: 1,
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-
-      }}>
-        <Grid container spacing={2}>
-          <Grid item xs={9} md={3}>
-            <ButtonBase sx={{
-              height: "100%",
-              width: "auto",
-              marginTop: "0px",
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center center"
-            }}>
-              <Img src={Carimg} alt="carimg" />
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} md={9} sm container sx={{ paddingLeft: "20px", margin: "auto" }}>
-            <Grid item xs container direction="column" spacing={2} sx={{ justifyContent: "center", margin: "auto" }}>
-              <Grid item xs container spacing={2}>
-                <Grid xs={12} md={8} sx={{ padding: "2px" }}>
-                  <Grid spacing={2} container sx={{ width: "100%", display: "flex", flexWrap: "wrap", boxSizing: "border-box", marginTop: "10px", marginRight: "25px" }}>
-                    <Grid item>
-                      <Typography sx={{ background: "yellow" }}>Category N</Typography>
-                    </Grid>
-                    <Grid item>
-                      L
-                    </Grid>
-                    <Grid item>
-                      Andover
-                    </Grid>
-                  </Grid>
-                  <Typography variant='h6' sx={{
-                    fontSize: "1.25rem",
-                    fontFamily: "inter",
-                    fontWeight: 500,
-                    lineHeight: 1.6
-                  }}>
-                    Skoda Fabia ala bala
-                  </Typography>
-                </Grid>
-                <Grid item xs={12} md={4} sx={{ padding: "8px" }}>
-                  <Grid container sx={{ width: "100%", display: "flex", flexWrap: "wrap", boxSizing: "border-box" }}>
-                    <Grid xs={6} sm={6} md={12}>
-                      <AvTimerIcon sx={{ display: "inline-block", fontSize: "20px", paddingLeft: "2px" }} />
-                      <span style={{ marginLeft: "5px" }}>22,555</span>
-                    </Grid>
-                    <Grid xs={6} sm={6} md={12}>
-                      <CheckBoxIcon sx={{ display: "inline-block", fontSize: "20px", paddingLeft: "2px" }} />
-                      <span>Starts</span>
-                    </Grid>
-                    <Grid xs={6} sm={6} md={12}>
-                      <CheckBoxIcon sx={{ display: "inline-block", fontSize: "20px", paddingLeft: "2px" }} />
-                      <span>Drives</span>
-                    </Grid>
-                    <Grid xs={6} sm={6} md={12}>
-                      <CheckBoxIcon sx={{ display: "inline-block", fontSize: "20px", paddingLeft: "2px" }} />
-                      <span>Ref:12211</span>
-
-                    </Grid>
-
-                  </Grid>
-
-                </Grid>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}><hr /></Grid>
-
-                </Grid>
-                <Grid container spacing={2} sx={{ minHeight: "32px" }}>
-
-                  <Grid item xs={12} sm={3} sx={{ margin: "auto", paddingRight: "100px" }}>
-                    <Typography variant='h5' sx={{ color: "orange" }}>
-                      £800
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12} sm={5} sx={{ margin: "auto", padding: "4px" }}>
-                    <Grid container xs={12} spacing={2} sx={{ width: "calc(100% + 16px)", margin: "-8px" }}>
-                      <Grid item sx={{ padding: "8px" }}>
-                        24th Oct 2023 13:04
+            <Grid item xs={12} md={9} sm container sx={{ paddingLeft: "20px", margin: "auto" }}>
+              <Grid item xs container direction="column" spacing={2} sx={{ justifyContent: "center", margin: "auto" }}>
+                <Grid item xs container spacing={2}>
+                  <Grid xs={12} md={8} sx={{ padding: "2px" }}>
+                    <Grid spacing={2} container sx={{ width: "100%", display: "flex", flexWrap: "wrap", boxSizing: "border-box", marginTop: "10px", marginRight: "25px" }}>
+                      <Grid item>
+                        <Typography sx={{ background: "yellow" }}>Category N</Typography>
+                      </Grid>
+                      <Grid item>
+                        L
+                      </Grid>
+                      <Grid item>
+                        Andover
                       </Grid>
                     </Grid>
+                    <Typography variant='h6' sx={{
+                      fontSize: "1.25rem",
+                      fontFamily: "inter",
+                      fontWeight: 500,
+                      lineHeight: 1.6
+                    }}>
+                      {car.year} {car.mark} {car.model}, {car.color}
+                    </Typography>
                   </Grid>
-                  <Grid item xs={12} sm={4} sx={{ margin: "auto", padding: "4px" }}>
-                    <Button variant="contained" sx={{ color: "white", background: "orange" }} >
-                      Place Bid
-                    </Button>
+                  <Grid item xs={12} md={4} sx={{ padding: "8px" }}>
+                    <Grid container sx={{ width: "100%", display: "flex", flexWrap: "wrap", boxSizing: "border-box" }}>
+                      <Grid xs={6} sm={6} md={12}>
+                        <AvTimerIcon sx={{ display: "inline-block", fontSize: "20px", paddingLeft: "2px" }} />
+                        <span style={{ marginLeft: "5px" }}>22,555</span>
+                      </Grid>
+                      <Grid xs={6} sm={6} md={12}>
+                        <CheckBoxIcon sx={{ display: "inline-block", fontSize: "20px", paddingLeft: "2px" }} />
+                        <span>Starts</span>
+                      </Grid>
+                      <Grid xs={6} sm={6} md={12}>
+                        <CheckBoxIcon sx={{ display: "inline-block", fontSize: "20px", paddingLeft: "2px" }} />
+                        <span>Drives</span>
+                      </Grid>
+                      <Grid xs={6} sm={6} md={12}>
+                        <CheckBoxIcon sx={{ display: "inline-block", fontSize: "20px", paddingLeft: "2px" }} />
+                        <span>Ref:12211</span>
 
+                      </Grid>
+
+                    </Grid>
 
                   </Grid>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}><hr /></Grid>
+                  </Grid>
+                  <Grid container spacing={2} sx={{ minHeight: "32px" }}>
 
+                    <Grid item xs={12} sm={3} sx={{ margin: "auto", paddingRight: "100px" }}>
+                      <Typography variant='h5' sx={{ color: "orange" }}>
+                        £800
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={5} sx={{ margin: "auto", padding: "4px" }}>
+                      <Grid container xs={12} spacing={2} sx={{ width: "calc(100% + 16px)", margin: "-8px" }}>
+                        <Grid item sx={{ padding: "8px" }}>
+                          24th Oct 2023 13:04
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                    <Grid item xs={12} sm={4} sx={{ margin: "auto", padding: "4px" }}>
+                      <Button variant="contained" sx={{ color: "white", background: "orange" }} >
+                        Place Bid
+                      </Button>
+
+
+                    </Grid>
+
+                  </Grid>
                 </Grid>
+
               </Grid>
 
             </Grid>
 
           </Grid>
 
-        </Grid>
+        </Paper>
 
-      </Paper>
+      ))}
+
+
 
     </div >
   );
