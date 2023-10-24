fa26a889c34c7873428e6f9311a4739b93907b64
Radoslav22 <4takvr26@solent.ac.uk>
2023-10-24 12:42:30 +0100

    Displaying of cars successfully created

--- client/src/components/carlist.js
+++ client/src/components/carlist.js
@@ -123,7 +123,17 @@ export default function ComplexGrid() {
         </Paper >
       ))}
 
-      <Box>
+      <Paper sx={{
+        p: 2,
+        margin: 'auto',
+        marginTop: 2,
+        marginBottom: 2,
+        maxWidth: "80%",
+        flexGrow: 1,
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+
+      }}>
         <Grid container spacing={2}>
           <Grid item xs={9} md={3}>
             <ButtonBase sx={{
@@ -137,8 +147,8 @@ export default function ComplexGrid() {
               <Img src={Carimg} alt="carimg" />
             </ButtonBase>
           </Grid>
-          <Grid item xs={12} md={9} sm container sx={{ padding: "20px" }}>
-            <Grid item xs container direction="column" spacing={2} sx={{ justifyContent: "center" }}>
+          <Grid item xs={12} md={9} sm container sx={{ paddingLeft: "20px", margin: "auto" }}>
+            <Grid item xs container direction="column" spacing={2} sx={{ justifyContent: "center", margin: "auto" }}>
               <Grid item xs container spacing={2}>
                 <Grid xs={12} md={8} sx={{ padding: "2px" }}>
                   <Grid spacing={2} container sx={{ width: "100%", display: "flex", flexWrap: "wrap", boxSizing: "border-box", marginTop: "10px", marginRight: "25px" }}>
@@ -179,24 +189,38 @@ export default function ComplexGrid() {
                       <CheckBoxIcon sx={{ display: "inline-block", fontSize: "20px", paddingLeft: "2px" }} />
                       <span>Ref:12211</span>
 
-                    </Grid><Grid container xs={1} spacing={2}>
-                      <Grid item xs={12} sm={3}>
-                        <Typography variant='h5' sx={{ color: "orange" }}>
-                          850
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={12} sm={5}>
-                        <Grid container xs={2} spacing={2}>
-                          <Typography variant='h6' >
-                            21st Oct 2023 13:00
-                          </Typography>
-                        </Grid>
-                      </Grid>
                     </Grid>
+
                   </Grid>
 
                 </Grid>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}><hr /></Grid>
 
+                </Grid>
+                <Grid container spacing={2} sx={{ minHeight: "32px" }}>
+
+                  <Grid item xs={12} sm={3} sx={{ margin: "auto", paddingRight: "100px" }}>
+                    <Typography variant='h5' sx={{ color: "orange" }}>
+                      £800
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={5} sx={{ margin: "auto", padding: "4px" }}>
+                    <Grid container xs={12} spacing={2} sx={{ width: "calc(100% + 16px)", margin: "-8px" }}>
+                      <Grid item sx={{ padding: "8px" }}>
+                        24th Oct 2023 13:04
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12} sm={4} sx={{ margin: "auto", padding: "4px" }}>
+                    <Button variant="contained" sx={{ color: "white", background: "orange" }} >
+                      Place Bid
+                    </Button>
+
+
+                  </Grid>
+
+                </Grid>
               </Grid>
 
             </Grid>
@@ -205,7 +229,7 @@ export default function ComplexGrid() {
 
         </Grid>
 
-      </Box>
+      </Paper>
 
     </div >
   );
